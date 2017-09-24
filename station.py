c4ff04186641569cab5eccebf03807214e0ac5d8
Eddie Richter <edwardrichter@email.arizona.edu>
2017-09-24 01:46:22 -0700

    Adding first class

---
+++ station.py
@@ -0,0 +1,14 @@
+class station
+	
+	def __init__(self):
+		self.back_off = 0
+		self.poisson_parameter = 0
+		print 'In constructor of station'
+
+
+def main():
+	print 'In the main of station.py'
+
+
+if __name__ == '__main__':
+	main()
